5f1e32fc705c4fbdeb61f1bb030fc0c9ca36606b
rifftyo <rifftyo@gmail.com>
2024-08-29 12:46:20 +0700

    Update Provider Cache

--- lib/provider/cnn_news_provider.dart
+++ lib/provider/cnn_news_provider.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:news_app/data/api/api_service.dart';
 import 'package:news_app/data/model/news_new_cnn.dart';
@@ -10,8 +9,9 @@ class CnnNewsProvider extends ChangeNotifier {
 
   CnnNewsProvider({required this.apiService});
 
+  final Map<String, NewsNewCnn> _cacheNews = {};
   late NewsNewCnn _newsList;
-  ResultState _state = ResultState.loading;
+  ResultState _state = ResultState.noData;
   String _message = '';
 
   String get message => _message;
@@ -22,42 +22,51 @@ class CnnNewsProvider extends ChangeNotifier {
 
   Future<dynamic> fetchListCnn(String category) async {
     try {
-      _state = ResultState.loading;
-      notifyListeners();
-      final NewsNewCnn newsData;
-
-      switch (category) {
-        case 'Ekonomi':
-          newsData = await apiService.getListNewsEconomyCnn();
-          break;
-        case 'Nasional':
-          newsData = await apiService.getListNewsNasionalCnn();
-          break;
-        case 'Internasional':
-          newsData = await apiService.getListNewsInternasionalCnn();
-          break;
-        case 'Olahraga':
-          newsData = await apiService.getListNewsSportCnn();
-          break;
-        case 'Teknologi':
-          newsData = await apiService.getListNewsTechnologyCnn();
-          break;
-        case 'Hiburan':
-          newsData = await apiService.getListNewsEntertainCnn();
-          break;
-        default:
-          newsData = await apiService.getListNewsNewCnn();
-          break;
-      }
-
-      if (newsData.data.posts.isEmpty) {
-        _state = ResultState.noData;
-        notifyListeners();
-        return _message = 'Empty Data';
-      } else {
+      if (_cacheNews.containsKey(category)) {
+        _newsList = _cacheNews[category]!;
         _state = ResultState.hasData;
         notifyListeners();
-        return _newsList = newsData;
+        return _newsList;
+      } else {
+        _state = ResultState.loading;
+        notifyListeners();
+        final NewsNewCnn newsData;
+
+        switch (category) {
+          case 'Ekonomi':
+            newsData = await apiService.getListNewsEconomyCnn();
+            break;
+          case 'Nasional':
+            newsData = await apiService.getListNewsNasionalCnn();
+            break;
+          case 'Internasional':
+            newsData = await apiService.getListNewsInternasionalCnn();
+            break;
+          case 'Olahraga':
+            newsData = await apiService.getListNewsSportCnn();
+            break;
+          case 'Teknologi':
+            newsData = await apiService.getListNewsTechnologyCnn();
+            break;
+          case 'Hiburan':
+            newsData = await apiService.getListNewsEntertainCnn();
+            break;
+          default:
+            newsData = await apiService.getListNewsNewCnn();
+            break;
+        }
+
+        if (newsData.data.posts.isEmpty) {
+          _state = ResultState.noData;
+          notifyListeners();
+          return _message = 'Empty Data';
+        } else {
+          _state = ResultState.hasData;
+          _newsList = newsData;
+          _cacheNews[category] = newsData;
+          notifyListeners();
+          return _newsList;
+        }
       }
     } catch (e) {
       _state = ResultState.error;
