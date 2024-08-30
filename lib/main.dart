fbd1f04edb0e2edf27629b0fb51676b672084c6c
rifftyo <rifftyo@gmail.com>
2024-08-30 13:42:05 +0700

    Change Title

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Newsly App',
+      title: 'Newsly',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
