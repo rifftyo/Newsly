835855fed2f1b93192a02e30ca050933a013cfec
rifftyo <rifftyo@gmail.com>
2024-08-31 11:46:00 +0700

    Add Sign Page

---
+++ lib/ui/signup_page.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/ui/login_page.dart';
+
+class SignupPage extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  SignupPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Center(
+              child: Text(
+                'Sign Up',
+                style: TextStyle(
+                  fontWeight: FontWeight.w800,
+                  fontSize: 45,
+                ),
+              ),
+            ),
+            const SizedBox(height: 40),
+            Center(
+                child: Image.asset('assets/news_icon.png',
+                    width: 100, height: 100)),
+            const SizedBox(height: 50),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 25),
+              child: TextField(
+                controller: _emailController,
+                onChanged: (value) {},
+                decoration: InputDecoration(
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none,
+                  ),
+                  hintText: "Type your email",
+                ),
+              ),
+            ),
+            const SizedBox(height: 25),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 25),
+              child: TextField(
+                obscureText: true,
+                controller: _passwordController,
+                onChanged: (value) {},
+                decoration: InputDecoration(
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none,
+                  ),
+                  hintText: "Type your password",
+                ),
+              ),
+            ),
+            const SizedBox(height: 25),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 25),
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    )),
+                child: const Text(
+                  'Login',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Have an account?  ',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w800,
+                    fontSize: 15,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => LoginPage()));
+                  },
+                  child: const Text(
+                    'Sign In',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w800,
+                      fontSize: 15,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
