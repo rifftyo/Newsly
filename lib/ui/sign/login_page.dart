a4b041a03e9f116aae623aa38e078b0311792e28
rifftyo <rifftyo@gmail.com>
2024-09-03 16:59:01 +0700

    Update Login Page

--- lib/ui/sign/login_page.dart
+++ lib/ui/sign/login_page.dart
@@ -1,129 +1,171 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:news_app/services/auth_service.dart';
+import 'package:news_app/provider/auth_provider_sign.dart';
 import 'package:news_app/ui/main/home_page.dart';
 import 'package:news_app/ui/sign/signup_page.dart';
+import 'package:provider/provider.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  final AuthService _authService = AuthService();
 
-  LoginPage({super.key});
+  final TextEditingController _passwordController = TextEditingController();
+
+  bool isSeen = false;
+
+  void changeVisibility() {
+    setState(() {
+      isSeen = !isSeen;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            const Center(
-              child: Text(
-                'Login',
-                style: TextStyle(
-                  fontWeight: FontWeight.w800,
-                  fontSize: 45,
-                ),
-              ),
-            ),
-            const SizedBox(height: 40),
-            Center(
-                child: Image.asset('assets/news_icon.png',
-                    width: 100, height: 100)),
-            const SizedBox(height: 50),
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 25),
-              child: TextField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide.none,
-                  ),
-                  hintText: "Type your email",
-                ),
-              ),
-            ),
-            const SizedBox(height: 25),
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 25),
-              child: TextField(
-                obscureText: true,
-                controller: _passwordController,
-                decoration: InputDecoration(
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide.none,
-                  ),
-                  hintText: "Type your password",
-                ),
-              ),
-            ),
-            const SizedBox(height: 25),
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 25),
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () async {
-                  String email = _emailController.text.trim();
-                  String password = _passwordController.text.trim();
-
-                  User? user = await _authService.signInWithEmailAndPassword(
-                      email, password);
-
-                  if (user != null) {
-                    Navigator.pushReplacement(context,
-                        MaterialPageRoute(builder: (context) => HomePage()));
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text(
-                            'Login gagal, pastikan email telah diverifikasi.'),
+    return ChangeNotifierProvider(
+      create: (_) => AuthProviderSign(),
+      child: Scaffold(
+        body: SafeArea(
+          child: Consumer<AuthProviderSign>(
+            builder: (context, authProvider, _) {
+              return Stack(
+                children: [
+                  Column(
+                    children: [
+                      const Center(
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w800,
+                            fontSize: 45,
+                          ),
+                        ),
                       ),
-                    );
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    foregroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    )),
-                child: const Text(
-                  'Login',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Don\'t have an account?  ',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w800,
-                    fontSize: 15,
+                      const SizedBox(height: 40),
+                      Center(
+                          child: Image.asset('assets/news_icon.png',
+                              width: 100, height: 100)),
+                      const SizedBox(height: 50),
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 25),
+                        child: TextField(
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                            filled: true,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide.none,
+                            ),
+                            hintText: "Type your email",
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 25),
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 25),
+                        child: TextField(
+                          obscureText: isSeen ? false : true,
+                          controller: _passwordController,
+                          decoration: InputDecoration(
+                            filled: true,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide.none,
+                            ),
+                            suffixIcon: IconButton(
+                                onPressed: changeVisibility,
+                                icon: isSeen
+                                    ? const Icon(Icons.visibility)
+                                    : const Icon(Icons.visibility_off)),
+                            hintText: "Type your password",
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 25),
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 25),
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: authProvider.isLoading
+                              ? null
+                              : () async {
+                                  String email = _emailController.text.trim();
+                                  String password =
+                                      _passwordController.text.trim();
+
+                                  User? user = await authProvider.signIn(
+                                      email, password);
+
+                                  if (user != null && context.mounted) {
+                                    Navigator.pushReplacement(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                const HomePage()));
+                                  } else if (context.mounted) {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      const SnackBar(
+                                        content: Text(
+                                            'Login gagal, pastikan email telah diverifikasi.'),
+                                      ),
+                                    );
+                                  }
+                                },
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blue,
+                              foregroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              )),
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Don\'t have an account?  ',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w800,
+                              fontSize: 15,
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => SignupPage()));
+                            },
+                            child: const Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w800,
+                                fontSize: 15,
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => SignupPage()));
-                  },
-                  child: const Text(
-                    'Sign Up',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w800,
-                      fontSize: 15,
-                      color: Colors.blue,
+                  if (authProvider.isLoading)
+                    const Center(
+                      child: CircularProgressIndicator(),
                     ),
-                  ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
