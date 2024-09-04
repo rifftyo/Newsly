369a8ce838ff533bbc18360da639618e4e264720
rifftyo <rifftyo@gmail.com>
2024-09-04 20:21:45 +0700

    Update Navigator

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:news_app/ui/main/home_page.dart';
 import 'package:news_app/ui/sign/login_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,17 +12,28 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  Future<void> _navigateToNextPage() async {
+    await Future.delayed(const Duration(milliseconds: 3500));
+
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    bool isLoggedIn = preferences.getBool('isLoggedIn') ?? false;
+
+    if (mounted) {
+      if (isLoggedIn) {
+        Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => const HomePage()));
+      } else {
+        Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => const LoginPage()));
+      }
+    }
+  }
+
   @override
   void initState() {
     super.initState();
     SchedulerBinding.instance.addPostFrameCallback((_) {
-      Future.delayed(const Duration(milliseconds: 3500), () {
-        if (mounted) { // Pastikan widget masih mounted sebelum navigasi
-          Navigator.of(context).pushReplacement(
-            MaterialPageRoute(builder: (context) => LoginPage()),
-          );
-        }
-      });
+      _navigateToNextPage();
     });
   }
 
