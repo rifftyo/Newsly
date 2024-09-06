490b85f5a2ad95c25be59ef941c73887c4cf9942
rifftyo <rifftyo@gmail.com>
2024-09-06 19:51:59 +0700

    Add Service Provider

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,12 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   // Pendaftaran Pengguna
   Future<User?> registerWithEmailAndPassword(
-      String email, String password) async {
+      String email, String password, String username) async {
     try {
       UserCredential result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
@@ -14,6 +16,17 @@ class AuthService {
 
       // Kirim email verifikasi
       await user?.sendEmailVerification();
+
+      // Simpan data ke firestore
+      if (user != null) {
+        await _firestore.collection('users').doc(user.uid).set({
+          'username': username,
+          'email': email,
+          'createdAt': FieldValue.serverTimestamp(),
+        });
+        saveLoginStatus();
+      }
+
       return user;
     } catch (e) {
       print(e.toString());
@@ -58,7 +71,7 @@ class AuthService {
   Future<bool> isEmailVerified(User user) async {
     await user.reload();
     User? updatedUser = _auth.currentUser;
-    
+
     return updatedUser != null && updatedUser.emailVerified;
   }
 
@@ -66,5 +79,21 @@ class AuthService {
   Future<void> saveLoginStatus() async {
     SharedPreferences preferences = await SharedPreferences.getInstance();
     preferences.setBool('isLoggedIn', true);
+  }
+
+  Future<String?> getUsername(String uid) async {
+    try {
+      DocumentSnapshot documentSnapshot =
+          await _firestore.collection('users').doc(uid).get();
+
+      if (documentSnapshot.exists) {
+        return documentSnapshot['username'];
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
   }
 }
