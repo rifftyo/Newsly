4c9c2ff0192ba348f79e9f318e75bd8cc8bf2504
rifftyo <rifftyo@gmail.com>
2024-09-08 19:31:26 +0700

    Add Form Validator Sign In

--- lib/ui/sign/login_page.dart
+++ lib/ui/sign/login_page.dart
@@ -18,6 +18,7 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _passwordController = TextEditingController();
 
   bool isSeen = false;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   void changeVisibility() {
     setState(() {
@@ -35,128 +36,165 @@ class _LoginPageState extends State<LoginPage> {
             builder: (context, authProvider, _) {
               return Stack(
                 children: [
-                  Column(
-                    children: [
-                      const Center(
-                        child: Text(
-                          'Login',
-                          style: TextStyle(
-                            fontWeight: FontWeight.w800,
-                            fontSize: 45,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 40),
-                      Center(
-                          child: Image.asset('assets/news_icon.png',
-                              width: 100, height: 100)),
-                      const SizedBox(height: 50),
-                      Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 25),
-                        child: TextField(
-                          controller: _emailController,
-                          decoration: InputDecoration(
-                            filled: true,
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide.none,
-                            ),
-                            hintText: "Type your email",
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 25),
-                      Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 25),
-                        child: TextField(
-                          obscureText: isSeen ? false : true,
-                          controller: _passwordController,
-                          decoration: InputDecoration(
-                            filled: true,
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide.none,
-                            ),
-                            suffixIcon: IconButton(
-                                onPressed: changeVisibility,
-                                icon: isSeen
-                                    ? const Icon(Icons.visibility)
-                                    : const Icon(Icons.visibility_off)),
-                            hintText: "Type your password",
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 25),
-                      Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 25),
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          onPressed: authProvider.isLoading
-                              ? null
-                              : () async {
-                                  String email = _emailController.text.trim();
-                                  String password =
-                                      _passwordController.text.trim();
-
-                                  User? user = await authProvider.signIn(
-                                      email, password);
-
-                                  if (user != null && context.mounted) {
-                                    Navigator.pushReplacement(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                const HomePage()));
-                                  } else if (context.mounted) {
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                      const SnackBar(
-                                        content: Text(
-                                            'Login gagal, pastikan email telah diverifikasi.'),
-                                      ),
-                                    );
-                                  }
-                                },
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.blue,
-                              foregroundColor: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15),
-                              )),
-                          child: const Text(
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        const Center(
+                          child: Text(
                             'Login',
-                            style: TextStyle(fontSize: 20),
-                          ),
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text(
-                            'Don\'t have an account?  ',
                             style: TextStyle(
                               fontWeight: FontWeight.w800,
-                              fontSize: 15,
+                              fontSize: 45,
                             ),
                           ),
-                          TextButton(
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => SignupPage()));
+                        ),
+                        const SizedBox(height: 40),
+                        Center(
+                            child: Image.asset('assets/news_icon.png',
+                                width: 100, height: 100)),
+                        const SizedBox(height: 50),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 25),
+                          child: TextFormField(
+                            controller: _emailController,
+                            decoration: InputDecoration(
+                              filled: true,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: BorderSide.none,
+                              ),
+                              hintText: "Type your email",
+                            ),
+                            validator: (value) {
+                              if (value == null || value.trim().isEmpty) {
+                                return 'Email harus diisi';
+                              }
+                              if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
+                                  .hasMatch(value)) {
+                                return 'Format email tidak valid';
+                              }
+                              return null;
                             },
+                          ),
+                        ),
+                        const SizedBox(height: 25),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 25),
+                          child: TextFormField(
+                            obscureText: isSeen ? false : true,
+                            controller: _passwordController,
+                            decoration: InputDecoration(
+                              filled: true,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: BorderSide.none,
+                              ),
+                              suffixIcon: IconButton(
+                                  onPressed: changeVisibility,
+                                  icon: isSeen
+                                      ? const Icon(Icons.visibility)
+                                      : const Icon(Icons.visibility_off)),
+                              hintText: "Type your password",
+                            ),
+                            validator: (value) {
+                              // Memeriksa apakah password diisi atau tidak
+                              if (value == null || value.isEmpty) {
+                                return 'Password tidak boleh kosong';
+                              }
+                              // Memeriksa apakah panjang password kurang dari 8 karakter
+                              if (value.length < 8) {
+                                return 'Password minimal harus 8 karakter';
+                              }
+                              // Memeriksa apakah password mengandung huruf kapital
+                              if (!RegExp(r'[A-Z]').hasMatch(value)) {
+                                return 'Password harus mengandung setidaknya 1 huruf kapital';
+                              }
+                              // Memeriksa apakah password mengandung angka
+                              if (!RegExp(r'[0-9]').hasMatch(value)) {
+                                return 'Password harus mengandung setidaknya 1 angka';
+                              }
+                              return null; // Jika semua validasi lolos
+                            },
+                          ),
+                        ),
+                        const SizedBox(height: 25),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 25),
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            onPressed: authProvider.isLoading
+                                ? null
+                                : () async {
+                                    if (_formKey.currentState?.validate() ??
+                                        false) {
+                                      String email =
+                                          _emailController.text.trim();
+                                      String password =
+                                          _passwordController.text.trim();
+
+                                      User? user = await authProvider.signIn(
+                                          email, password);
+
+                                      if (user != null && context.mounted) {
+                                        Navigator.pushReplacement(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    const HomePage()));
+                                      } else if (context.mounted) {
+                                        ScaffoldMessenger.of(context)
+                                            .showSnackBar(
+                                          const SnackBar(
+                                            content: Text(
+                                                'Login gagal, pastikan email telah diverifikasi.'),
+                                          ),
+                                        );
+                                      }
+                                    }
+                                  },
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.blue,
+                                foregroundColor: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                )),
                             child: const Text(
-                              'Sign Up',
+                              'Login',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              'Don\'t have an account?  ',
                               style: TextStyle(
                                 fontWeight: FontWeight.w800,
                                 fontSize: 15,
-                                color: Colors.blue,
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                    ],
+                            TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => const SignupPage()));
+                              },
+                              child: const Text(
+                                'Sign Up',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w800,
+                                  fontSize: 15,
+                                  color: Colors.blue,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                   if (authProvider.isLoading)
                     const Center(
